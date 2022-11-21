f162294ef538f0cf3b6b81a98ab1fc53ff72bc72
João Loureiro <joaoloureiro2002@gmail.com>
2022-11-21 14:13:13 +0000

    s

--- Game/func.py
+++ Game/func.py
@@ -16,8 +16,26 @@ class Display:
                     g.MAP[y][x] = " "
                 elif pix[x,y] == (255,0,0):
                     g.MAP[y][x] = g.PLAYER_SKIN
+                    g.PLAYER_X = x
+                    g.PLAYER_Y = y
                 row = row + g.MAP[y][x] 
             print(row)
     def display_options():
         print("\n")
-        print("Options:\nUse de arrow to move around the screen but will cost you a turn\nOr see your items with the key I")
+        print("Options:\nUse de wasd to move around the screen but will cost you a turn\nOr see your items with the key I and then press enter")
+    def update_map():
+        g.MAP[g.PLAYER_Y,g.PLAYER_X] = " "
+        file = os.path.join("Assets","Maps","level"+str(g.LEVEL)+".gif")
+        img = Image.open(file).convert("RGB")
+        pix = img.load()
+        for y in range(15):
+            row =""
+            for x in range(100):
+                if pix[x,y] == (0,0,0):
+                    g.MAP[y][x] = "█"                
+                elif pix[x,y] == (255,255,255):
+                    g.MAP[y][x] = " "
+                if (x == g.PLAYER_X and g.PLAYER_Y == y):
+                    g.MAP[y][x] = g.PLAYER_SKIN
+                row = row + g.MAP[y][x]
+            print(row)       
--- Game/main.py
+++ Game/main.py
@@ -6,11 +6,15 @@ res = lambda : os.system('mode con: cols=100 lines=30')
 
 
 game_Start = True
-
+Display.draw_raw_Map()
 while (game_Start and not g.dead):
     res()
+    #fix
     Display.draw_raw_Map()
     Display.display_options()
     
-
-    Pause = input("")
+    Option = input("")
+    if Option == "d":
+        g.PLAYER_X = g.PLAYER_X + 1
+    Display.update_map()
+    Display.display_options()
